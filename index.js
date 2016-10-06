b9a999e3d8c54872b6f6a6eb76b2383c841b256e
MFogleman <marcfogleman@gmail.com>
2016-10-06 18:38:06 -0400

    Added nodemailer to modules, modified index.js to iterate over multiple users

--- index.js
+++ index.js
@@ -1,25 +1,30 @@
 "use strict";
 
-//Twilio credentials and user phone number stored in environment variables for security.
+//Twilio credentials and phone number stored in environment variables for security.
 const accountSid = process.env['TWILIO_ACCOUNT_SID'];
 const authToken = process.env['TWILO_AUTH_TOKEN'];
 const twilioNum = process.env['TWILIONUM'];
-const userNum = process.env['USERNUM'];
 
-var userName = "Marc"
+const client = require('twilio')(accountSid, authToken);
+const mockDB = require('./mockDB.js');
 
-var client = require('twilio')(accountSid, authToken);
 
-client.messages.create({
-    to: userNum,
-    from: twilioNum,
-    body: 'Twilio check addressed to ' + userName,
-},  
-    function (err, message){
-        if (err){
-            console.log("Error! ", err);
-        }else{
-            console.log("Message succesfully sent to ", userNum, " from ",twilioNum);
+for (let i=0; i < mockDB.users.length; i++){
+    sendSMS(mockDB.users[i]);
+}
+
+function sendSMS(surveyor){
+    client.messages.create({
+        to: surveyor.userPhone,
+        from: twilioNum,
+        body: 'Twilio check addressed to ' + surveyor.userName,
+    },  
+        function (err, message){
+            if (err){
+                console.log("Error! ", err);
+            }else{
+                console.log("Message succesfully sent to ", surveyor.userPhone, " from ",twilioNum);
+            }
         }
-    }
-);
+    );
+}

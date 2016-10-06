86c430e9bfeb83090630393173dc14b99afe5611
MFogleman <marcfogleman@gmail.com>
2016-10-06 12:55:35 -0400

    Cleaned index.js, added  mockDB.js (used for testing purposes) to .gitignore

--- index.js
+++ index.js
@@ -1,24 +1,25 @@
 "use strict";
 
-//Twilio credentials stored in a separate file for git security
-//const auth = require('./auth.js');
-
+//Twilio credentials and user phone number stored in environment variables for security.
 const accountSid = process.env['TWILIO_ACCOUNT_SID'];
 const authToken = process.env['TWILO_AUTH_TOKEN'];
 const twilioNum = process.env['TWILIONUM'];
 const userNum = process.env['USERNUM'];
 
-//require the Twilio module and create a REST client
-//const client = require('twilio')(accountSid, authToken);
+var userName = "Marc"
+
 var client = require('twilio')(accountSid, authToken);
+
 client.messages.create({
     to: userNum,
     from: twilioNum,
-    body: 'Twilio check with environment variables',
-}, function (err, message){
-    if (err){
-        console.log("Error!  Err: ", err, " Message.sid: ", message.sid);
-    }else{
-        console.log("Message succesfully sent to ", userNum, " from ",twilioNum);
+    body: 'Twilio check addressed to ' + userName,
+},  
+    function (err, message){
+        if (err){
+            console.log("Error! ", err);
+        }else{
+            console.log("Message succesfully sent to ", userNum, " from ",twilioNum);
+        }
     }
-});
+);
